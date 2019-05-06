7661d19db1cf5bfca51b49043ac3c358eeed130d
cevmadra <45848353+cevmadra@users.noreply.github.com>
2019-05-06 09:34:15 +0000

    Update lab3.hs

--- lab3.hs
+++ lab3.hs
@@ -59,3 +59,61 @@ print $ getByName testData "Obama B.H."
 print $ getByName testData "KolodeznyDiver"
 print $ getByLetter testData 'P'
 print $ getAssignment testData (4, 8,1961)
+
+
+type Name = String
+type Day = Integer
+type Month = Integer
+type Year = Integer
+type Birthday = (Day, Month, Year)
+type Phone = String
+type Description = String
+
+data Records = B1 Name  Birthday
+             | P1 Name Phone
+             | Meeting Date Description deriving (Eq, Show)
+
+type NoteBook = [Records]
+
+getPhoneByName :: Name -> NoteBook -> Maybe Phone
+getPhoneByName _ [] = Noting 
+getPhoneByName n (x:xs) = case x of
+    Birthday nc bd -> if n == nc then Just bd
+                                 else getVithdayByName n xs
+                    -> getBirthdayByName n xs
+                    
+getByName  :: Name -> NoteBook ->  (Maybe Phone, Maybe Birthday)
+getByName n nb = (getPhoneByName n nb, getBirthdayByName n nb)
+
+{- 2) -}
+
+getByLetter :: Char -> NoteBook -> [Name]
+getByLetter _ [] = []
+getByLetter c (x:xs) = case x of
+    Birthday n _ -> if head n == c then n:getByLetter c xs
+                                   else getByLetter c xs
+    Phone n _ ->  if head n == c then n:getByLetter c xs
+                                 else getByLetter c xs
+             -> getByLetter c xs
+             
+getAssignment :: Date -> NoteBook -> ([Description], [Phone])
+getAssignment d nb = 
+    let getMeetingByDate :: Date -> Notebook -> [Description]
+        getMeetingByDate - [] = []
+        getMeetingByDate d (x:xs) = case x of 
+            Meeting dc ds -> if d == dc then ds:getMeetingByDate d xs
+                                        else getMeetingByDate d xs
+                        -> getMeetingByDate d xs
+                        
+getPhoneByDate :: Date -> NoteBook -> NoteBook -> [Phone]
+getPhoneByDate - [] = []
+getPhoneByDate (d,m,y) (x:xs) nb = case x of
+    Birthday n (dc,mc) -> if (d == dc) && (m == mc)
+                            then case getPhoneByName n nb of
+                                Just p -> (n ++ ':' :p) :getPhoneByDate (d,m ,y) xs nb
+                                    -> )(n ++ " - no phone"): getPhoneByDate (d,m ,y) xs nb
+                            else getPhoneByDate (d,m,y) xs nb
+                  -> getPhoneByDate (d,m,y) xs nb
+    in (getMeetingByDate d nb, getPhoneByDate d nb nb)
+    
+    
